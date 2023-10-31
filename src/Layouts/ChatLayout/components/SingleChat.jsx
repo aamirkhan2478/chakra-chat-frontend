6235c2b524c540bb1ae732bd30c107e300005b02
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-10-31 18:19:34 +0500

    Fix socketio url

--- src/Layouts/ChatLayout/components/SingleChat.jsx
+++ src/Layouts/ChatLayout/components/SingleChat.jsx
@@ -20,7 +20,7 @@ import io from "socket.io-client";
 import Lottie from "react-lottie";
 import animationData from "../assets/animations/typing.json";
 
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://chakra-chat-backend.vercel.app";
 var socket, selectedChatCompare;
 
 const SingleChat = ({
